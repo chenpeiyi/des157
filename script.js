b96f9a9bb37d154bd3af2a0430cdfd08032b9216
chenpeiyi <cpychen@ucdavis.edu>
2017-01-23 22:07:23 -0800

    added js comment for banner

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-// p5 file
+// reviewed processing and make an interactive banner
+// as hover over, create random ellipses
+// colors are of blue and orange tone, matching the style of the website
+// right click to clear canvas
 
-// open the console window of any browser to see console.log() messages,
-// like print() messages in Processing
 console.log("this is a js comment");
 
-function setup() {
+function setup() {  //set up canvas
   background(255);
   var myCanvas=createCanvas(800, 250);
   myCanvas.parent('mySketch');
   frameRate(5);
-
 }
 
 function draw() {
   colorMode(HSB, 360, 100, 100);
-  fill(random(20, 200),random(70,72), 89, .2);
+  fill(random(20, 200),random(70,72), 89, .2);  // constrain color to blue-orange
   noStroke();
-  ellipse(mouseX, mouseY, random(50,400), random(100,400));
+  ellipse(mouseX, mouseY, random(50,400), random(100,400)); // draw ellipses
 
-  if (mouseIsPressed){
+  if (mouseIsPressed){    // start over if mouse is pressed
     background(255);
   }
 
