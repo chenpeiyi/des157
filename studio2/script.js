88012b4a3942c54c37f64b0ba8944de1a54177cc
chenpeiyi <cpychen@ucdavis.edu>
2017-01-30 23:07:16 -0800

    done, working on gif

--- studio2/script.js
+++ studio2/script.js
@@ -26,21 +26,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
         // if it passed error detection, write my message in html
         else {
             var myMsg = document.getElementById('myMsg');
-            myMsg.innerHTML = '<ol><li>Go to ' + userNum + ' percent of your class. </li>' +
+            myMsg.innerHTML = '<li>Go to <em>' + userNum + '</em> percent of your class. </li>' +
 
-                '<li>When in class, always catch people’s attention by yelling out to your professor: “I love ' + userProf + '."</li>' +
+                '<li>When in class, always catch people’s attention by yelling out to your professor: “I love <em>' + userProf + '</em>."</li>' +
 
-                '<li>Get to know all your neighbors, including the cutest ' + userAnimal + ' on your way to class.</li>' +
+                '<li>Get to know all your neighbors, including the cutest <em>' + userAnimal + '</em> on your way to class.</li>' +
 
-                '<li>Go to your professor’s office hours! Make sure you ask them for ' + userFood + '.</li>' +
+                '<li>Go to your professor’s office hours! Make sure you ask them for <em>' + userFood + '</em>.</li>' +
 
-                '<li>Miss your parents? Call them ' + userCall + ' times a day and say "I love you" to them.' +
+                '<li>Miss your parents? Call them <em>' + userCall + '</em> times a day and say "I love you" to them.</li>' +
 
-                '<li>Too Asian to say "I love you"? Ask ' + userBFF + ' to say it for you. Or talk to them about your stress and homesick-ness. They are always there for you! &#10084 </li>';
+                '<li>Too Asian to say "I love you"? Ask <em>' + userBFF + '</em> to say it for you. Or talk to them about your stress and homesick-ness. They are always there for you! &#10084 </li>';
 
             //show it by referencing to the css
             myMsg.className = 'show';
-        }
+            /*document.img.style.className = 'show';    */    
+          }
 
         return false;
     }
