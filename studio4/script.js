92caf7a0bbb5d27ff2dfb66d88562957f2ddd28f
chenpeiyi <cpychen@ucdavis.edu>
2017-02-14 13:03:56 -0800

    edited

--- studio4/script.js
+++ studio4/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
 
-    var imgs = ["images/car.png","images/car_blue.png", "images/roof.png", "images/roof2.png", "images/roof_green.png", "images/roof_pink.png", "images/housebod.png", "images/housebodem.png",  "images/housebod_green.png", "images/housebod_pink.png", "images/girl.png", "images/girl_org.png"];
+    var imgs = ["images/car.png", "images/car_blue.png", "images/roof.png", "images/roof2.png", "images/roof_green.png", "images/roof_pink.png", "images/housebod.png", "images/housebodem.png", "images/housebod_green.png", "images/housebod_pink.png", "images/girl.png", "images/girl_org.png"];
 
     // an empty array that holds the images as js objects
     var preload = [];
@@ -42,122 +42,133 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var divs = [cardiv, funeraldiv, girldiv, housediv];
     var pics = ['car', 'roof1', 'roof2', 'bod1', 'girl', 'roof2', 'bod2'];
 
+
+    // change pic functions
     cardiv.addEventListener('mouseover', changePic1);
-    function changePic1(){
-      car.src="images/car_blue.png";
-      console.log("changing car");
-      tipTimer = setTimeout(showCarTip, 1500);
-      console.log("showing car tip")
+
+    function changePic1() {
+        car.src = "images/car_blue.png";
+        console.log("changing car");
+        tipTimer = setTimeout(showCarTip, 1500);
+        console.log("showing car tip")
     }
-
-    cardiv.addEventListener ('mouseout', function(){
-      console.log('mouseout on Car');
-      clearTimeout(tipTimer);
-      carTip.style.opacity = 0;
-    });
-
-    function showCarTip(){
-      carTip.style.opacity = 1;
-      carTip.style.transition = 'all .5s';
-    }
-
-    cardiv.addEventListener ('click', function(){
-      carAnim = requestAnimationFrame(moveCar);
-      });
-
-      var newLeft = 353;
-      function moveCar() {
-        console.log('moving dot');
-        var w = window.innerWidth;
-        var h = window.innerHeight;
-        console.log ('w: ' + w + ', h: ' + h);
-          newLeft ++;
-          car.style.left = newLeft + 'px';
-          console.log ('newLeft: ' + newLeft);
-          dotAnim = requestAnimationFrame(moveCar);
-      }
-
-
-/*
-    cardiv.addEventListener('mousedown', moveCar);
-    function m
-    oveCar(){
-
-    }*/
 
     bod1.addEventListener('mouseover', changePic2);
-    function changePic2(){
-      roof1.src="images/roof_pink.png";
-      bod1.src="images/housebod_pink.png";
-      tipTimer = setTimeout(showFuneralTip, 1500 );
-      console.log("changing house1 funeral");
-    }
 
-    bod1.addEventListener ('mouseout', function(){
-      console.log('mouseout on house1');
-      clearTimeout(tipTimer);
-      funeralTip.style.opacity = 0;
-    });
-
-    function showFuneralTip(){
-      funeralTip.style.opacity = 1;
-      funeralTip.style.transition = 'all .5s';
+    function changePic2() {
+        roof1.src = "images/roof_pink.png";
+        bod1.src = "images/housebod_pink.png";
+        tipTimer = setTimeout(showFuneralTip, 1500);
+        console.log("changing house1 funeral");
     }
 
     girl.addEventListener('mouseover', changePic3);
-    function changePic3(){
-      girl.src="images/girl_org.png";
-      tipTimer = setTimeout(showGirlTip, 1500);
-      console.log("changing girl");
+
+    function changePic3() {
+        girl.src = "images/girl_org.png";
+        tipTimer = setTimeout(showGirlTip, 1500);
+        console.log("changing girl");
     }
-
-    girl.addEventListener ('mouseout', function(){
-      console.log('mouseout on girl');
-      clearTimeout(tipTimer);
-      girlTip.style.opacity = 0;
-    });
-
-    function showGirlTip(){
-      girlTip.style.opacity = 1;
-      girlTip.style.transition = 'all .5s';
-    }
-
 
     bod2.addEventListener('mouseover', changePic4);
-    function changePic4(){
-      roof2.src="images/roof_green.png";
-      bod2.src="images/housebod_green.png";
-      tipTimer = setTimeout(showHouseTip, 1500);
-      console.log("changing house2 empty");
+
+    function changePic4() {
+        roof2.src = "images/roof_green.png";
+        bod2.src = "images/housebod_green.png";
+        tipTimer = setTimeout(showHouseTip, 1500);
+        console.log("changing house2 empty");
     }
 
-    bod2.addEventListener ('mouseout', function(){
-      console.log('mouseout on house2');
-      clearTimeout(tipTimer);
-      houseTip.style.opacity = 0;
+
+    /*
+        for (var i = 0; i < pics.length; i++){
+          pics[i].addEventListener('mouseover', function(){ //addEventListener is not a function !?!??!?
+          console.log(pics[i]);
+          console.log("mouseover");
+        })}
+
+        */
+
+
+    cardiv.addEventListener('mouseout', function() {
+        console.log('mouseout on Car');
+        clearTimeout(tipTimer);
+        carTip.style.opacity = 0;
     });
 
-    function showHouseTip(){
-      houseTip.style.opacity = 1;
-      houseTip.style.transition = 'all .5s';
+    function showCarTip() {
+        carTip.style.opacity = 1;
+        carTip.style.transition = 'all .5s';
     }
 
 
+    girl.addEventListener('mouseout', function() {
+        console.log('mouseout on girl');
+        clearTimeout(tipTimer);
+        girlTip.style.opacity = 0;
+    });
 
+    function showGirlTip() {
+        girlTip.style.opacity = 1;
+        girlTip.style.transition = 'all .5s';
+    }
 
+    bod2.addEventListener('mouseout', function() {
+        console.log('mouseout on house2');
+        clearTimeout(tipTimer);
+        houseTip.style.opacity = 0;
+    });
 
+    function showHouseTip() {
+        houseTip.style.opacity = 1;
+        houseTip.style.transition = 'all .5s';
+    }
 
+    bod1.addEventListener('mouseout', function() {
+        console.log('mouseout on house1');
+        clearTimeout(tipTimer);
+        funeralTip.style.opacity = 0;
+    });
 
-/*
-    for (var i = 0; i < pics.length; i++){
-      pics[i].addEventListener('mouseover', function(){ //addEventListener is not a function !?!??!?
-      console.log(pics[i]);
-      console.log("mouseover");
-    })}
+    function showFuneralTip() {
+        funeralTip.style.opacity = 1;
+        funeralTip.style.transition = 'all .5s';
+    }
 
-    */
+    // animation starts
+    cardiv.addEventListener('click', function() {
+        carAnim = requestAnimationFrame(moveCar);
+    });
 
+    var carNewLeft = 353;
 
+    function moveCar() {
+        console.log('moving car');
+        var w = window.innerWidth;
+        var h = window.innerHeight;
+        console.log('w: ' + w + ', h: ' + h);
+        carNewLeft++;
+        car.style.left = carNewLeft + 'px';
+        console.log('newLeft: ' + carNewLeft);
+        carAnim = requestAnimationFrame(moveCar);
 
+        if (carNewLeft == w - 500){
+          carNewLeft --;
+        }
+    }
 
-  });
+    bod1.addEventListener('click', function() {
+        house1Anim = requestAnimationFrame(moveHouse1);
+    });
+
+    var roof1top = 285;
+    var bod1top = 383;
+
+    function moveHouse1(){
+        console.log('moving house1');
+        var w = window.innerWidth;
+        var h = window.innerHeight;
+        house1Anim = requestAnimationFrame(moveHouse1);
+    }
+
+});
