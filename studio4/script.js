7c54fcfa5f6a2e38427424d5c6616c8c133739bc
chenpeiyi <cpychen@ucdavis.edu>
2017-02-13 21:13:49 -0800

    uploaded images

--- studio4/script.js
+++ studio4/script.js
@@ -14,3 +14,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
         preload[i].src = imgs[i];
         console.log(preload[i]);
     }
+
+    var car = document.getElementById('car');
+    var roof1 = document.getElementById('roof1');
+    var bod1 = document.getElementById('bod1');
+    var girl = document.getElementById('girl');
+    var roof2 = document.getElementById('roof2');
+    var bod2 = document.getElementById('bod2');
+
+    var pics = [car, roof1, roof2, bod1, girl, roof2, bod2];
