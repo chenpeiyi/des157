7e62f242a4f6d6cbfbec0f2f7014222a42da9ac4
chenpeiyi <cpychen@ucdavis.edu>
2017-02-22 23:12:55 -0800

    moving, trying to position

--- studio5/script.js
+++ studio5/script.js
@@ -15,19 +15,36 @@ document.addEventListener("DOMContentLoaded", function(event) {
         console.log(preload[i]);
     }
 
+
+    var scene = document.getElementById('scene');
+    var parallax = new Parallax(scene, {
+        calibrateX: false,
+        calibrateY: true,
+        invertX: true,
+        invertY: true,
+        limitX: false,
+        limitY: false,
+        scalarX: 10.0,
+        scalarY: 10.0,
+        frictionX: 0.1,
+        frictionY: 0.1,
+        originX: 0.5,
+        originY: 0.5,
+    });
+/*
     document.f.onsubmit = processForm;
 
-    function processForm(){
-      var score = 0;
-      var petp = document.f.petp.checked;
-      var million = document.f.million.checked;
-      var pls = document.f.pls.checked;
+    function processForm() {
+        var score = 0;
+        var petp = document.f.petp.checked;
+        var million = document.f.million.checked;
+        var pls = document.f.pls.checked;
 
-      console.log("pet peeve "+ petp);
-      console.log("million "+ million);
-      console.log("pls "+ pls);
+        console.log("pet peeve " + petp);
+        console.log("million " + million);
+        console.log("pls " + pls);
 
 
-    }
+    }*/
 
 });
