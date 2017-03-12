b4e5cb9eb5d30a4caa2acfe6bcbc6650930d6336
chenpeiyi <cpychen@ucdavis.edu>
2017-03-11 22:49:42 -0800

    proto3

--- project/proto3/script.js
+++ project/proto3/script.js
@@ -1,8 +1,45 @@
 // JavaScript Document
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
 
-    // all other js here
+    var signInButton = document.getElementById("sign-in-button");
+    var signOutButton = document.getElementById("sign-out-button");
+    var currentUser = document.getElementById("current-user");
+    var userId;
+
+    signInButton.addEventListener('click', function(){
+
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithRedirect(provider);
+
+    });
+
+    signOutButton.addEventListener('click', function(){
+
+      firebase.auth().signOut();
+
+    });
+
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+
+        currentUser.innerHTML = user.displayName;
+        userId = user.displayName;
+        console.log(userId);
+
+      } else {
+
+        currentUser.innerHTML = "null";
+      }
+});
 
 });
